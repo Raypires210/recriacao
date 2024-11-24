f95f5eb5fa76163bf7526b04c31781710b214cc2
Rayana Pires <rayanapiress210@gmail.com>
2024-11-24 01:41:12 -0300

    recriando

---
+++ java.js
@@ -0,0 +1,66 @@
+var moldura
+var fotos
+var indice
+var temporizador
+var botaoVoltar
+var botaoAvancar
+
+const tempointervalo = 3000
+
+botaoVoltar = document.getElementById("btnVoltar");
+botaoAvancar = document.getElementById("btnAvancar");
+moldura = document.getElementById("moldura")
+
+indice = 0
+
+fotos = ["baner.jpeg", "mulher.png"]
+
+window.onload = inicio()
+
+function inicio() {
+   botaoAvancar.onclick=Avancarfoto
+   botaoVoltar.onclick=Voltarfoto
+   mostrafoto()
+   Automaticofoto()
+
+}
+
+function Avancarfoto() {
+    if(indice<fotos.length-1){
+        indice++
+    }
+    else{
+        indice=0
+    }
+
+    mostrafoto()
+    clearInterval(temporizador)
+    Automaticofoto()
+}
+
+function Voltarfoto() {
+    if(indice >0)
+    {
+        indice--
+    }
+    else{
+        indice=fotos.length-1
+    }
+    mostrafoto()
+    clearInterval(temporizador)
+    Automaticofoto()
+}
+
+function Automaticofoto(){
+    temporizador=setInterval(Avancarfoto,tempointervalo)
+}
+
+function mostrafoto() {
+    moldura.src = "img/" + fotos[indice]
+}
+
+
+// script.js
+document.getElementById("menu").addEventListener("click", function() {
+    document.getElementById("sidebar").classList.toggle("active");
+});
